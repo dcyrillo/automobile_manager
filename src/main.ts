8708c6feec2f94d6068fa97c4c6c056aaf2edf5d
Carlos Eduardo Magalhães Cyrillo <carlos.cyrillo@haizen.tech>
2021-02-05 19:41:33 -0300

    feat(automobiles): increment endpoint automobiles  - method create automobiles  - method update automobiles

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 
