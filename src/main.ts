232883ae48e861931fdb2cf305749173a92c8354
Carlos Eduardo Magalhães Cyrillo <carlos.cyrillo@haizen.tech>
2021-02-05 18:30:05 -0300

    chore: improving swagger api
    - add project's owner
    - add automobiles endpoint into documentation

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Automomobile management')
+    .setDescription('The automomobile management API description')
+    .setContact(
+      'Carlos Eduardo Magalhaes Cyrillo',
+      'https://github.com/dcyrillo',
+      'd.cyrillo@hotmail.com',
+    )
     .setVersion('1.0.0')
-    .addTag('cats')
+    .addTag('automobiles')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
