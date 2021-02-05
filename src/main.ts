7db6cb99a48a625cf921af7d9bbfcc18a968108e
Carlos Eduardo Magalhães Cyrillo <carlos.cyrillo@haizen.tech>
2021-02-05 18:14:30 -0300

    chore: setting swagger 	-fix docker commands cleaning cache

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const options = new DocumentBuilder()
-    .setTitle('Automobile Management')
-    .setDescription('The automobile management API description')
-    .setContact(
-      'Carlos Eduardo Magalhaes Cyrillo',
-      'https://www.linkedin.com/in/carlos-eduardo-magalh%C3%A3es-cyrillo-a24ab216a/',
-      'd.cyrillo@hotmail.com',
-    )
-    .setVersion('1.0.0')
-    .addTag('automobiles')
-    .build();
+  const app = await NestFactory.create(AppModule);
 
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const document = SwaggerModule.createDocument(app, options);
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
+
+  //  app.useGlobalPipes(new ValidationPipe());
 }
 bootstrap();
